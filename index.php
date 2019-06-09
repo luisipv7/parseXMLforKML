4616904dc8b9c6e54b6b46a619075b2173e0d897
LuisFernandoSantos <32277920+luisipv7@users.noreply.github.com>
2019-06-09 12:23:33 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
    <input type="file" class="form-control-file" name="arquivo"><br><br>
    <input type="text" name ="nome" size="50" placeholder = "Digite o titulo da caixa EX: CTO; BAR; MCP.....">
     <input type="submit"   class="btn btn-success"value="Enviar">
+    <br><a href="../lerxml/nap.kml">Baixar NAP</a>
 </form> <br><br><br><hr>
 
 <form action="default.php" method="POST">
@@ -21,7 +22,7 @@
 <br><a href="../lerxml/infradefault.kml">Baixar infradefault</a>
 
 <br><br><br>
-<form method="POST" action="processa.php" enctype="multipart/form-data">
+<form method="POST" action="lerxmltotal.php" enctype="multipart/form-data">
    <label for="">Contagem de itens</label><br><hr>
     <input type="file" class="form-control-file" name="arquivo"><br><br>
     <input type="submit"   class="btn btn-success"value="Enviar">
